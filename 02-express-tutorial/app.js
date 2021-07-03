b8dee71d0c3fc13b505bcfd5027f968de164df8b
irah rosete <irah.rosete@outlook.com>
2021-07-04 00:52:37 +1000

    complete http request

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,7 +2,21 @@ const http = require('http')
 
 const server = http.createServer((req, res) => {
   console.log('user hit the server')
-  res.end('Home Page')
+
+  const url = req.url
+  if (req.url === '/') {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    res.write('<h1>Home Page</h1>')
+    res.end()
+  } else if (req.url === '/about') {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    res.write('<h1>About Page</h1>')
+    res.end()
+  } else {
+    res.writeHead(404, { 'content-type': 'text/html' })
+    res.write('<h1>File Not Found</h1>')
+    res.end()
+  }
 })
 
 server.listen(4000)

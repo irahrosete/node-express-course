116d408c601314f05f40f05530c893a88a5162a1
irah rosete <irah.rosete@outlook.com>
2021-07-05 00:00:12 +1000

    complete method PUT form example

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,8 +2,19 @@ const express = require('express')
 const app = express()
 const { people } = require('./data')
 
+app.use(express.static('./methods-public'))
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => {
   res.status(200).json({ success: true, data: people })
+})
+
+app.post('/login', (req, res) => {
+  const { name } = req.body
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`)
+  }
+  res.status(404).send('Please enter name')
 })
 
 app.listen(5000, () => {

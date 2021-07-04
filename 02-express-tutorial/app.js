c954d7b944c2957e72f59a6ccb5f233e0596c76d
irah rosete <irah.rosete@outlook.com>
2021-07-05 02:31:25 +1000

    fix methods POST and PUT and complete express router

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-const { people } = require('./data')
+
+const people = require('./routes/people')
+const auth = require('./routes/auth')
 
 app.use(express.static('./methods-public'))
 // parse form data to make req.body possible
@@ -8,73 +10,8 @@ app.use(express.urlencoded({ extended: false }))
 // parse json to make req.body possible
 app.use(express.json())
 
-app.get('/api/people', (req, res) => {
-  res.status(200).json({ success: true, data: people })
-})
-
-// CHECK! POST is not working
-app.post('api/people', (req, res) => {
-  const { name } = req.body
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: 'Please provide a name' })
-  }
-  res.status(201).json({ success: true, person: name })
-})
-
-app.post('api/postman/people', (req, res) => {
-  const { name } = req.body
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: 'Please provide a name' })
-  }
-  res.status(201).json({ success: true, data: [...people, name] })
-})
-
-app.post('/login', (req, res) => {
-  const { name } = req.body
-  if (name) {
-    return res.status(200).send(`Welcome ${name}`)
-  }
-  res.status(400).send('Please enter name')
-})
-
-// CHECK! PUT is not working
-app.put('api/people/:id', (req, res) => {
-  const { id } = req.params
-  const { name } = req.body
-
-  const person = people.find((person) => person.id === Number(id))
-
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `no person with id ${id}` })
-  }
-  const newPeople = people.map((person) => {
-    if (person.id === Number(id)) {
-      person.name = name
-    }
-    return person
-  })
-  res.status(200).json({ success: true, data: newPeople })
-})
-
-app.delete('/api/people/:id', (req, res) => {
-  const person = people.find((person) => person.id === Number(req.params.id))
-
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `no person with id ${req.params.id}` })
-  }
-  const newPeople = people.filter((person) => {
-    return person.id !== Number(req.params.id)
-  })
-  return res.status(200).json({ success: true, data: newPeople })
-})
+app.use('/login', auth)
+app.use('/api/people', people)
 
 app.listen(5000, () => {
   console.log('server listening on port 5000...')

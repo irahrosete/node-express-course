30220d1d93dad9a4944bd01473a03205456c0010
irah rosete <irah.rosete@outlook.com>
2021-07-04 16:39:41 +1000

    complete json params, query string setup

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -3,7 +3,15 @@ const app = express()
 const { products } = require('./data')
 
 app.get('/', (req, res) => {
-  res.json(products)
+  res.send('<h1>Home Page</h1><a href="/api/products">Products</a>')
+})
+
+app.get('/api/products', (req, res) => {
+  const newProducts = products.map((item) => {
+    const { id, name, image } = item
+    return { id, name, image }
+  })
+  res.json(newProducts)
 })
 
 app.listen(5000, () => {

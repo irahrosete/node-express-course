e7f61f8e36de27eebb5c51397e0e0a13c1da61d7
irah rosete <irah.rosete@outlook.com>
2021-07-04 22:55:24 +1000

    complete app.use

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,19 +1,14 @@
 const express = require('express')
 const app = express()
+const logger = require('./logger')
 
-const logger = (req, res, next) => {
-  const method = req.method
-  const url = req.url
-  const date = new Date().getFullYear()
-  console.log(method, url, date)
-  next()
-}
+app.use(logger)
 
-app.get('/', logger, (req, res) => {
+app.get('/', (req, res) => {
   res.send('Home')
 })
 
-app.get('/about', logger, (req, res) => {
+app.get('/about', (req, res) => {
   res.send('About')
 })
 

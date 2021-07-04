77f09ed73aeac2643a1400c594c29330f7493f29
irah rosete <irah.rosete@outlook.com>
2021-07-04 23:37:59 +1000

    complete method GET

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,25 +1,9 @@
 const express = require('express')
 const app = express()
-const logger = require('./logger')
-const authorise = require('./authorise')
-
-// apply to all paths
-app.use([logger, authorise])
-// only apply to specific path
-// app.use('/about', logger)
+const { people } = require('./data')
 
 app.get('/', (req, res) => {
-  console.log(req.user)
-  res.send('Home')
-})
-
-app.get('/about', (req, res) => {
-  res.send('About')
-})
-
-// apply both middleware to this specific path
-app.get('/api/items', [logger, authorise], (req, res) => {
-  res.send('Items')
+  res.status(200).json({ success: true, data: people })
 })
 
 app.listen(5000, () => {
